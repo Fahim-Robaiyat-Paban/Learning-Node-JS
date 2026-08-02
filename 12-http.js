5d1d8811a1ee257ca4abe3a7dc84c980cd12b47d
Fahim-Robaiyat-Paban <fahimrobaiyatpaban@gmail.com>
2026-08-02 17:34:02 +0600

    sdfG

--- 12-http.js
+++ 12-http.js
@@ -19,3 +19,18 @@ const server = http.createServer((req,res) =>{
 })
 
 server.listen(6700)
+
+
+const server = http.createServer((req,res) => {
+    
+    if (req.url === '/') {
+        console.log('requested something');
+        res.end(`<h1>This is the home page</h1>`)
+    }
+    
+})
+
+server.listen(6700, () => {
+    console.log("Server listening on : 5000...");
+    
+})
--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 const http = require('http')
 
-const server = http.createServer((req,res) => {
-    
-    if (req.url === '/') {
-        console.log('requested something');
-        res.end(`<h1>This is the home page</h1>`)
-    }
-    
-})
-
-server.listen(6700, () => {
-    console.log("Server listening on : 5000...");
-    
-})
