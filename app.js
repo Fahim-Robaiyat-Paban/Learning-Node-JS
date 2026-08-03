1f62224f6d6a88cf9ccbae21859bff306b80761a
Fahim-Robaiyat-Paban <fahimrobaiyatpaban@gmail.com>
2026-08-03 07:14:50 +0600

    yuj

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const http = require('http')
-
-const serverrr = http.createServer((req,res) => {
-    if (req.url === '/') {
-        console.log('requested something');
-        res.end(`<h1>This is the home page</h1>`)
-    }
-    
-})
-
-serverrr.on('finish', () => {
-    console.log('connected');
-    
-})
-
-serverrr.listen(5000)
+// streams
