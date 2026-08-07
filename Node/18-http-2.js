66aefa7fc2a85110301c1dba9bd1c320b718c2ed
Fahim-Robaiyat-Paban <fahimrobaiyatpaban@gmail.com>
2026-08-07 20:09:42 +0600

    dfgh

--- Node/18-http-2.js
+++ Node/18-http-2.js
@@ -1,15 +1,27 @@
 const http = require('http')
-
+const fs = require('fs')
+const htmlz = fs.readFileSync('../Ready website example/index.html')
+const csss = fs.readFileSync(`../Ready website example/css/style.css`)
 const server = http.createServer((req,res) => {
     let url = req.url
     if (url === '/') {
-        console.log('someone made a request');       
+        console.log('html done');       
         res.writeHead(200, {'content-type': 'text/html'})
-        res.write(`<h1>Hello World?</h1>`)
-        res.end
+        res.write(htmlz)
+        res.end()
     }
+    if (url === '/css/style.css') {
+        console.log('css done');       
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(csss)
+        res.end()
+    }
+    else{
+        res.end()
+    }
+    
+
 })
 server.listen(5000, () => {
     console.log('server is running on port 5000...');
-    
 })
